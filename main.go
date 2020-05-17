a9a8b38748e107a70175de5ecc5554f74c8ae25d
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-17 13:48:00 +0900

    Update

--- main.go
+++ main.go
@@ -15,7 +15,19 @@ func showInfo() {
 	legal := assimp.GetLegalString()
 	fmt.Println(legal)
 }
+func showModelInfo(model_filename string) {
+	meshes, err := assimp.ParseFile(model_filename)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, mesh := range meshes {
+		fmt.Printf("[Mesh %v]\n", i)
+		fmt.Printf("Vertex num: %v\n", mesh.VertexCount)
+	}
+}
 
 func main() {
-	showInfo()
+	//showInfo()
+	showModelInfo("model.x")
 }
